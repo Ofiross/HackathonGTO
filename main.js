f889f61ef2b494b60bc3f54325201be01765a069
Gleb Omarov <gleb.omarov@gmail.com>
2022-01-29 12:00:00 +0200

    gleb

--- main.js
+++ main.js
@@ -43,47 +43,70 @@ const goal = 30;
 const recommendedThrows = 4;
 const scoreToStop = 15;
 const scoreToContinue = 5;
+let tryesOfAI = 0;
 // const robotThinking = setTimeout(function() {console.log(`${currentPlayers[currentTurn].name} desided`);},300);
 
+let currentPlayerH2 = document.getElementById("currentPlayer");
+// currentPlayerH2.addEventListener("onchange",function () {console.log("sdfsf");})
 
+
+/*
+ *  Changes a player: Human, Computer, or None
+ */
+function changePlayer() {
+console.log("change player");
+}
 /*
  *  Start a game
  */
 function runGame() {
+    // makes all variables to 0;
+    currentPlayers = [];
     currentSum = 0;
-    //currentPlayers.push(player1); //temp
-    //currentPlayers.push(player2); //temp
-    currentPlayers.push(player3); //temp
-    currentPlayers.push(player4); //temp
-
-    currentPlayers.forEach(player => player.score = 0); 
-
-
-    //gets every player according buttons on form and appends them to array "currentPlayers"
-    //creates and arranges all divs on html
-    // console.log("start!"); temporary
-    currentTurn = Math.floor(Math.random() * currentPlayers.length); //temporary
-    console.log("currentTurn: ", currentPlayers[currentTurn].name);
-    if (currentPlayers[currentTurn].race == "Robot") {
-        strategyAI();
+    // creates players
+    for(i=1;i<=4;i++){
+        let race = document.getElementById("player"+ i).value;
+        if (race == "Human" || race == "Computer"){
+            currentPlayers.push(createPlayer("Player" + i, race))
+        }
     }
+    //creates and arranges all divs on html
+    arrangeDivs(currentPlayers.length);
+
+    currentTurn = Math.floor(Math.random() * currentPlayers.length);
+    console.log("currentTurn: ", currentPlayers[currentTurn].name);
 }
 
-/*
- *  Pass a turn
- */
-function passTurn() {
-    currentPlayers[currentTurn].score += currentSum;
-    currentSum = 0;
-    if (currentTurn < currentPlayers.length - 1)
-        currentTurn++;
-    else
-        currentTurn = 0;
-    // setActivePlayer(currentTurn); need to draw point of frames on html
-    console.log("currentTurn: ", currentPlayers[currentTurn].name);
-    if (currentPlayers[currentTurn].race == "Robot") {
-        strategyAI();
+function createPlayer(name, race){
+    let player = {
+        name: name,
+        race: race,
+        score: 0,
     }
+    return player;
+}
+
+function createDivs(num){
+    let
+}
+/*
+ * Make a move
+ */
+function makeMove() {
+    number = throwDice();
+    if (number == 1) {
+        eatScore(currentPlayers[currentTurn]);
+        passTurn();
+        return;
+    }
+    currentPlayers[currentTurn].currentThrows.push(number);
+    currentSum += number;
+    let espectedScore = currentPlayers[currentTurn].score + currentSum;
+    console.log("score is: ", currentPlayers[currentTurn].score, " current score is: ", currentSum, " espectedScore = ", espectedScore);
+    if (espectedScore >= goal) {
+        gameOver(currentPlayers[currentTurn]);
+    }
+    return;
 }
 
 /*
@@ -106,43 +129,47 @@ function eatScore(player) {
 }
 
 /*
- * Make a move
+ *  Pass a turn
  */
-function makeMove() {
-    number = throwDice();
-    // console.log(`test`);//temp
-    if (number == 1) {
-        eatScore(currentPlayers[currentTurn]);
-        passTurn();
-        return number;
-    }
-    currentPlayers[currentTurn].currentThrows.push(number);
-    currentSum += number;
-    let espectedScore = currentPlayers[currentTurn].score + currentSum;
-    console.log("score is: ", currentPlayers[currentTurn].score, " current score is: ", currentSum, " espectedScore = ", espectedScore);
-    if (espectedScore >= goal) {
-        gameOver(currentPlayers[currentTurn]);
-    }
-    return number;
+function passTurn() {
+    currentPlayers[currentTurn].score += currentSum;
+    currentSum = 0;
+    if (currentTurn < currentPlayers.length - 1)
+        currentTurn++;
+    else
+        currentTurn = 0;
+    // setActivePlayer(currentTurn); need to draw point of frames on html
+    console.log("currentTurn: ", currentPlayers[currentTurn].name);
+    currentPlayerH2.textContent = currentPlayers[currentTurn].race;
+
 }
 
+/*
+ * Game over
+ */
+function gameOver() {
+    // draws banner on html
+    console.log(`Game over. ${currentPlayers[currentTurn].name} won!`);
+    return;
+}
 
+//------------------------------------------------------------------------------------------------
 /*
  * AI strategy
  */
 function strategyAI() {
     for (let t = 0; t <= recommendedThrows; t++) {
-        
+
         makeMove();
         if (currentSum >= scoreToStop) {
             console.log(`${currentPlayers[currentTurn].name} prefers to pass`);
             passTurn();
-            return; 
+            return;
         }
         else if (number == 1) {
             return;
         }
-        else if ((currentPlayers[currentTurn].score + currentSum) >= goal) {    
+        else if ((currentPlayers[currentTurn].score + currentSum) >= goal) {
             // console.log(`${currentEePlayers[currentTurn].name} happy.`);
             return;
         }
@@ -156,38 +183,68 @@ function strategyAI() {
     }
 }
 
-/*
- * Game over
- */
-function gameOver() {
-    // draws banner on html
-    console.log(`Game over. ${currentPlayers[currentTurn].name} won!`);
-    return;
+function moveOfAI() {
+    if (tryesOfAI >= recommendedThrows || currentSum >= scoreToStop) {
+        console.log(`${currentPlayers[currentTurn].name} prefers to pass`);
+        passTurn();
+        tryesOfAI = 0;
+        return;
+    }
+    number = throwDice();
+    tryesOfAI++;
+    if (number == 1) {
+        eatScore(currentPlayers[currentTurn]);
+        tryesOfAI = 0;
+        passTurn();
+        return;
+    }
+    currentPlayers[currentTurn].currentThrows.push(number);
+    currentSum += number;
+    let espectedScore = currentPlayers[currentTurn].score + currentSum;
+    console.log("score is: ", currentPlayers[currentTurn].score, " current score is: ", currentSum, " espectedScore = ", espectedScore);
+    
+    if (espectedScore >= goal) {
+        gameOver(currentPlayers[currentTurn]);
+        return
+    }
 }
 
-function computerThinking(){
-    // pseudo func
-    console.log("hi");
-    return;
+function ifRobot(){
+    console.log("robot");
+    return true;
 }
+//------------------------------------------------------------------------------------
 
-
-function printGlobalScore(){
-    let score = currentPlayers.map(player => player.score);
-    console.log("Global score: ", score);
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+//creating variables for the correct x and y position to show the number.
+let one = 'rotateX(900deg) rotateY(900deg)';
+let two = 'rotateX(90deg) rotateY(990deg)';
+let three = 'rotateX(540deg) rotateY(450deg)';
+let four = 'rotateX(1080deg) rotateY(450deg)';
+let five = 'rotateX(990deg) rotateY(90deg)';
+let six = 'rotateX(1260deg) rotateY(720deg)';
+//the program which base on the random number assign the correct coordinates to show the right cube pattern
+let dice = () => {
+    let playetDice = Math.floor(Math.random() * 6) + 1;
+    switch (true) {
+        case (playetDice == 1):
+            cube.style.transform = `${one}`;
+            break;
+        case (playetDice == 2):
+            cube.style.transform = `${two}`;
+            break;
+        case (playetDice == 3):
+            cube.style.transform = `${three}`;
+            break;
+        case (playetDice == 4):
+            cube.style.transform = `${four}`;
+            break;
+        case (playetDice == 5):
+            cube.style.transform = `${five}`;
+            break;
+        case (playetDice == 6):
+            cube.style.transform = `${six}`;
+            break;
+    };
+};
+//calling the roll a dice funtion onClick
+document.getElementById('start').addEventListener('click', dice);
