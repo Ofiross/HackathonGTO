013aa668702677d854da06b873f96544b5d5a9ae
Ofiross <ofir23851@gmail.com>
2022-01-30 10:10:04 +0100

    30/01/2022

--- main.js
+++ main.js
@@ -2,26 +2,23 @@ let currentPlayers = [];
 let currentTurn = 0; // index of array "currentPlayers"
 let currentSum = 0;
 let number = 0;
-const goal = 50;
+const goal = 100;
 
 /* AI strategy variables */
-const recommendedThrows = 4;
-const scoreToStop = 15;
-const scoreToContinue = 5;
-let tryesOfAI = 0;
+const recommendedThrows = 3;
+const scoreToStop = 16;
+const scoreToContinue = 12;
+let aiTurns = 0;
 
-for (pl of document.getElementsByClassName("player-container")) {
-    pl.addEventListener("change", () => console.log("test"));
-    console.log(pl)
-}
-/****************************************************************************************************************** */
-/*
- *  Start a game
- */
 function runGame() {
+    onGame = true
+    console.log("New Game");
     // makes all variables to 0;
     currentPlayers = [];
+    currentTurn = 0;
     currentSum = 0;
+    number = 0;
+
     // creates players
     for (i = 1; i <= 4; i++) {
         let race = document.getElementById("player" + i + "_choise").value;
@@ -40,12 +37,15 @@ function runGame() {
     document.getElementById("playerchoise").style.display = "none";
     document.getElementById("startgame").style.display = "none";
     document.getElementById("roll").style.display = "";
+    document.getElementById("roll").disabled = false;
     document.getElementById("passturn").style.display = "";
     //make active-player
     currentTurn = Math.floor(Math.random() * currentPlayers.length);
     console.log("currentTurn: ", currentPlayers[currentTurn].name);
     makeActive();
+    document.getElementById('passturn').disabled = true;
 }
+
 
 function createPlayer(name, race) {
     let player = {
@@ -73,19 +73,35 @@ function arrangeDivs(num) {
 
 function makeActive() {
     active = document.getElementsByClassName("player-container")[currentTurn];
-    for (pl of document.getElementsByClassName("player-container"))
+    for (pl of document.getElementsByClassName("player-container")) {
         pl.classList.remove("player-active");
+    }
     active.classList.add("player-active");
+    if (currentPlayers[currentTurn].race == "computer") { // compurer's turn
+        console.log("Computer is thinking...")
+        if (currentSum == 0)
+            setTimeout(computersTurn, 1000);
+        else
+            setTimeout(computersTurn, 3000);
+        document.getElementById('passturn').disabled = true;
+        document.getElementById('roll').disabled = true;
+    }
+    else {
+        aiTurns = 0;
+        document.getElementById('passturn').disabled = false;
+        document.getElementById('roll').disabled = false;
+    }
+
 }
 
 /*
  * Make a move
  */
 function makeMove() {
-    number = throwDice();
+    throwDice();
     if (number == 1) {
         eatScore(currentPlayers[currentTurn]);
-        setTimeout(updateScore, 2500, 0);
+        setTimeout(updateScore, 1000, currentPlayers[currentTurn].score);
         passTurn();
         return;
     }
@@ -94,14 +110,11 @@ function makeMove() {
     let espectedScore = currentPlayers[currentTurn].score + currentSum;
     console.log("score is: ", currentPlayers[currentTurn].score, " current score is: ", currentSum, " espectedScore = ", espectedScore);
     //update score
-    setTimeout(updateScore, 2000, espectedScore);
+    setTimeout(updateScore, 1000, espectedScore);
     if (espectedScore >= goal) {
-        document.getElementById('gameEnd').style.display = 'block';
-        document.getElementById("victory").play();
-        gameOver(currentPlayers[currentTurn]);
-        return;
+        setTimeout(gameOver, 1000, currentPlayers[currentTurn]);
     }
-
+    return;
 }
 
 function updateScore(espectedScore) {
@@ -113,16 +126,21 @@ function updateScore(espectedScore) {
  *  Throws dice
  */
 function throwDice() {
-
     document.getElementById('passturn').disabled = true;
     document.getElementById('roll').disabled = true;
-    let number = Math.ceil(Math.random() * 6);
+    let oldNumber = number;
+    while (number == oldNumber) {
+        number = Math.ceil(Math.random() * 6);
+        if (number == 1) //decrease chance of 1 //temp
+            number = Math.ceil(Math.random() * 6);//temp    
+    }
+
     // rolling animation
     dice(number);
     document.getElementById('diceroll').play();
     console.log("Dice shows: ", number);
     setTimeout(resetCube, 4000)
-    setTimeout(activateButtons, 4000);
+    setTimeout(activateButtons, 2000);
     return number;
 }
 
@@ -161,11 +179,14 @@ function passTurn() {
  * Game over
  */
 function gameOver() {
+    document.getElementById('gameEnd').style.display = 'block';
+    document.getElementById("victory").play();
     // draws banner on html
     document.querySelector("#header>h1").textContent = `${currentPlayers[currentTurn].name} won!`;
     console.log(`Game over. ${currentPlayers[currentTurn].name} won!`);
     document.getElementById("roll").disabled = true;
     document.getElementById("passturn").disabled = true;
+    onGame = false;
     return;
 }
 
@@ -175,6 +196,8 @@ function gameOver() {
 function newGame() {
     document.getElementById('gameEnd').style.display = 'none'
     arrangeDivs(4);
+    currentSum = 0;
+
     //shows/hides buttons
     document.getElementById("playerchoise").style.display = "";
     document.getElementById("startgame").style.display = "";
@@ -184,79 +207,11 @@ function newGame() {
     for (player of document.querySelectorAll(".player-container")) {
         player.classList.remove("player-active");
     }
-    document.querySelectorAll(".currentscore").forEach(item => item.textContent = "current score: ");
-    document.querySelectorAll(".totalscore").forEach(item => item.textContent = "total score: ");
-    document.querySelector("#header>h1").textContent = "Piggies - A game of chance and greed";
+    document.querySelectorAll("#player1Current").forEach(item => item.textContent = "");
+    document.querySelectorAll("#player1Total").forEach(item => item.textContent = "");
+    document.querySelector("#header>h1").textContent = "PIGGIES - A game of chance and greed";
 }
 
-
-
-//------------------------------------------------------------------------------------------------
-function computerMoves() {
-    console.log("computer pass");
-    return false;
-}
-/*
- * AI strategy
- */
-function strategyAI() {
-    for (let t = 0; t <= recommendedThrows; t++) {
-
-        makeMove();
-        if (currentSum >= scoreToStop) {
-            console.log(`${currentPlayers[currentTurn].name} prefers to pass`);
-            passTurn();
-            return;
-        }
-        else if (number == 1) {
-            return;
-        }
-        else if ((currentPlayers[currentTurn].score + currentSum) >= goal) {
-            // console.log(`${currentEePlayers[currentTurn].name} happy.`);
-            return;
-        }
-        // else {
-        //     let chance = Math.random();
-        //     console.log(chance)
-        //     if ((t == recommendedThrows) &&  (chance < 0.95 )) { // one more try             ( currentSum <= scoreToContinue) ||
-        //     console.log("one more try");
-        //     t--;}
-        //}
-    }
-}
-
-function moveOfAI() {
-    if (tryesOfAI >= recommendedThrows || currentSum >= scoreToStop) {
-        console.log(`${currentPlayers[currentTurn].name} prefers to pass`);
-        passTurn();
-        tryesOfAI = 0;
-        return;
-    }
-    number = throwDice();
-    tryesOfAI++;
-    if (number == 1) {
-        eatScore(currentPlayers[currentTurn]);
-        tryesOfAI = 0;
-        passTurn();
-        return;
-    }
-    currentPlayers[currentTurn].currentThrows.push(number);
-    currentSum += number;
-    let espectedScore = currentPlayers[currentTurn].score + currentSum;
-    console.log("score is: ", currentPlayers[currentTurn].score, " current score is: ", currentSum, " espectedScore = ", espectedScore);
-
-    if (espectedScore >= goal) {
-        document.getElementById('gameEnd').style.display = 'block';
-        gameOver(currentPlayers[currentTurn]);
-        return
-    }
-}
-
-function ifRobot() {
-    console.log("robot");
-    return true;
-}
-//------------------------------------------------------------------------------------
 
 // program to roll a dice to show at the end the random number
 let cube = document.getElementById('gameCube');
@@ -273,6 +228,7 @@ let six = 'rotateX(1260deg) rotateY(720deg)';
 
 //the program which base on the random number assign the correct coordinates to show the right cube pattern
 let dice = (number) => {
+    document.getElementById('diceroll').play();
     switch (number) {
         case 1:
             cube.style.webkitTransform = `${one}`;
@@ -304,6 +260,59 @@ let dice = (number) => {
 let resetCube = () => {
     return cube.style.transform = 'none';
 };
+
+
+//-----------------------------------------------------------------------------------------------
+//------------------------------------------------------------------------------------------------
+
+function computersTurn() {
+    console.log("computersTurn: ", aiTurns)
+    if (computerPass()) {
+        console.log(`${currentPlayers[currentTurn].name}(Computer) passes the move`);
+        aiTurns = 0;
+        passTurn();
+        return;
+    }
+
+    throwDice();
+    if (number == 1) {
+        eatScore(currentPlayers[currentTurn]);
+        setTimeout(updateScore, 1000, currentPlayers[currentTurn].score);
+        passTurn();
+        return;
+    }
+
+    currentPlayers[currentTurn].currentThrows.push(number);
+    aiTurns++;
+    currentSum += number;
+    let espectedScore = currentPlayers[currentTurn].score + currentSum;
+    console.log("score is: ", currentPlayers[currentTurn].score, " current score is: ", currentSum, " espectedScore = ", espectedScore);
+    //update score
+    setTimeout(updateScore, 1000, espectedScore);
+
+    if (espectedScore >= goal) {
+        aiTurns = 0;
+        setTimeout(gameOver, 1000, currentPlayers[currentTurn]);
+        return;
+    }
+    if (aiTurns == recommendedThrows && currentSum <= scoreToContinue)
+        aiTurns--;
+
+    if (aiTurns >= recommendedThrows) {
+        aiTurns = 0;
+        setTimeout(passTurn, 2500);
+        console.log(`${currentPlayers[currentTurn].name}(Computer) passes the move`);
+    }
+    else
+        setTimeout(makeActive, 1000);
+}
+
+function computerPass() {
+    let risk = (Math.random < 0.85);
+    if (risk && currentSum >= scoreToStop)
+        console.log(`${currentPlayers[currentTurn].name}(Computer) takes a risk`);
+    return (currentSum >= scoreToStop) && risk // additional chance
+}
 
 /*********************************************AUDIO PLAYER************************************************ */
 let volumeOff = () => {
